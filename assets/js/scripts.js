d9de60fa88c0d142c8fd08b1d323c2ebc2337418
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-07-26 15:46:06 +0200

    add start Quiz function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -61,3 +61,10 @@ const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
